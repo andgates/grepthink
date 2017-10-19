94af49156557621779beb567ce491150ee66c633
Kevin Pannell <kapannel@ucsc.edu>
2017-10-18 21:44:24 -0700

    users can input 0-1mil if they want TODO

--- teamwork/apps/projects/forms.py
+++ teamwork/apps/projects/forms.py
@@ -397,9 +397,7 @@ class TSR(forms.ModelForm):
 
     perc_contribution = forms.DecimalField(
         widget=forms.NumberInput(attrs={'class': 'form-control'}),
-        label='% Contribution (Values between 0-99 only!)',
-        max_digits=2,
-        decimal_places=0,
+        label='% Contribution (Values between 0-99 only!)',        
         required=True)
 
     pos_fb = forms.CharField(
